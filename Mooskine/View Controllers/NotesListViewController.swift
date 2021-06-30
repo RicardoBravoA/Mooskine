83b6dbe93528e427d0c4d1f0d226878915737600
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-30 00:04:41 -0500

    feat: Add note

--- Mooskine/View Controllers/NotesListViewController.swift
+++ Mooskine/View Controllers/NotesListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class NotesListViewController: UIViewController, UITableViewDataSource {
     /// A table view that displays a list of notes for a notebook
@@ -16,6 +17,8 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
     var notebook: Notebook!
     
     var notes:[Note] = []
+    
+    var dataController: DataController!
 
     /// A date formatter for date text in note cells
     let dateFormatter: DateFormatter = {
@@ -29,6 +32,18 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
 
         navigationItem.title = notebook.name
         navigationItem.rightBarButtonItem = editButtonItem
+        
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        let predicate = NSPredicate(format: "notebook == %@", notebook)
+        fetchRequest.predicate = predicate
+        
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        if let result = try? dataController.viewContext.fetch(fetchRequest) {
+            notes = result
+        }
+        
         updateEditButtonState()
     }
 
@@ -53,8 +68,15 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
 
     // Adds a new `Note` to the end of the `notebook`'s `notes` array
     func addNote() {
-//        notebook.addNote()
-        tableView.insertRows(at: [IndexPath(row: numberOfNotes - 1, section: 0)], with: .fade)
+        let note = Note(context: dataController.viewContext)
+        note.text = "New note"
+        note.creationDate = Date()
+        note.notebook = notebook
+        try? dataController.viewContext.save()
+        
+        notes.insert(note, at: 0)
+        
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
         updateEditButtonState()
     }
 
