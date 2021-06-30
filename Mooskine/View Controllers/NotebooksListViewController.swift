a78f892c2f93111ea5d8b652e597f3118ec8f771
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-29 23:55:09 -0500

    feat: Delete notebook

--- Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine/View Controllers/NotebooksListViewController.swift
@@ -100,6 +100,10 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource {
 
     /// Deletes the notebook at the specified index path
     func deleteNotebook(at indexPath: IndexPath) {
+        let notebookDelete = notebook(at: indexPath)
+        dataController.viewContext.delete(notebookDelete)
+        try? dataController.viewContext.save()
+        
         notebooks.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
         if numberOfNotebooks == 0 {
