e951b847dfb98d0cbc272ccf53947e15331179cd
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-30 01:15:11 -0500

    feat: Relaod data with fetched results controller

--- Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine/View Controllers/NotebooksListViewController.swift
@@ -166,3 +166,32 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource, NSFe
         fetchedResultsController.delegate = self
     }
 }
+
+extension NotebooksListViewController {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+            break
+        case .delete:
+            tableView.deleteRows(at: [newIndexPath!], with: .fade)
+            break
+        case .update:
+            tableView.reloadRows(at: [newIndexPath!], with: .fade)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        default:
+            break
+        }
+    }
+    
+}
