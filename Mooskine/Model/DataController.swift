9c523c220b8d96385a3fdc9120bbf8453a67e7d3
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-30 00:26:43 -0500

    feat: Add auto save

--- Mooskine/Model/DataController.swift
+++ Mooskine/Model/DataController.swift
@@ -25,7 +25,24 @@ class DataController {
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
+            self.autoSAveViewContext()
             completion?()
+        }
+    }
+}
+
+extension DataController {
+    func autoSAveViewContext(interval: TimeInterval = 30) {
+        guard interval > 0 else {
+            print("Cannot set negative autosave interval")
+            return
+        }
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSAveViewContext(interval: interval)
         }
     }
 }
